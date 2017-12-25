13424747b0930220c7dabae6d852c4139d86505b
D.S <26146815+TheNyanta@users.noreply.github.com>
2017-12-25 11:51:57 +0100

    Add files via upload

--- world.js
+++ world.js
@@ -3,15 +3,13 @@ var rel_y = 0;
                                 
 var bergwald = new Sprite("ChipSet/bergwald.png", 16, 16, 30, 16);
 
-function DrawMap()
-{
-    // Draw Map from a single spritesheet + indexMap 
-    var bMapIndex = 0;        
+function DrawBackgroundMap() {
+    var mapIndex = 0;        
     for (var h = 0; h < 22; h++) {
-        for (var w = 0; w < 22; w++, bMapIndex++) {
+        for (var w = 0; w < 22; w++, mapIndex++) {
             var tile_w = (w + rel_x) * 16;
             var tile_h = (h + rel_y) * 16;
-            bergwald.draw(tile_w, tile_h, map[bMapIndex]-1);
+            bergwald.draw(tile_w, tile_h, map[mapIndex]-1);
         }
     }
 }
